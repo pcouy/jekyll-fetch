7d5121aeac520d5d8ec5ed1f1e664a64d01b33df
pcouy <couy.pierre@gmail.com>
2023-04-11 13:51:56 +0200

    Fix encoding errors for some URLs

--- lib/jekyll-fetch.rb
+++ lib/jekyll-fetch.rb
@@ -12,11 +12,12 @@ module Jekyll
       # You should choose better exception.
       raise ArgumentError, 'HTTP redirect too deep' if limit == 0
 
+      puts uri_str
       url = URI.parse(uri_str)
       req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (etc...)' })
       response = Net::HTTP.start(url.host, url.port, use_ssl: true) { |http| http.request(req) }
       case response
-      when Net::HTTPSuccess     then response.body
+      when Net::HTTPSuccess     then response.body.force_encoding("UTF-8")
       when Net::HTTPRedirection then fetch(response['location'], limit - 1)
       else
         response.error!
