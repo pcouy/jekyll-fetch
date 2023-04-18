a636e7a4336bb44e33b8e7e422e3f439359b9baa
pcouy <couy.pierre@gmail.com>
2023-04-18 10:01:34 +0200

    Refactor into shorter methods

--- lib/jekyll-fetch.rb
+++ lib/jekyll-fetch.rb
@@ -1,27 +1,49 @@
 # frozen_string_literal: true
 
-require_relative "jekyll-fetch/version"
-require "liquid"
-require "net/https"
+require_relative 'jekyll-fetch/version'
+require 'liquid'
+require 'net/https'
 
 module Jekyll
+  # Filters for transforming URL strings into the content at the URL
   module JekyllFetch
     class Error < StandardError; end
 
+    # Helper functions for HTTP requests
+    class Utils
+      class << self
+        def fetch(uri_str, limit = 10)
+          raise ArgumentError, 'HTTP redirect too deep' if limit.zero?
+
+          response_to_body(
+            uri_to_response(uri_str), limit - 1
+          )
+        end
+
+        private
+
+        def uri_to_response(uri_str)
+          url = URI.parse(uri_str)
+          req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (etc...)' })
+          Net::HTTP.start(url.host, url.port, use_ssl: true) { |http| http.request(req) }
+        end
+
+        def response_to_body(response, limit)
+          case response
+          when Net::HTTPSuccess     then response.body.force_encoding('UTF-8')
+          when Net::HTTPRedirection then fetch(response['location'], limit)
+          else
+            response.error!
+          end
+        end
+      end
+    end
+
     def fetch(uri_str, limit = 10)
-      # You should choose better exception.
-      raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+      raise ArgumentError, 'HTTP redirect too deep' if limit.zero?
 
       puts uri_str
-      url = URI.parse(uri_str)
-      req = Net::HTTP::Get.new(url.path, { 'User-Agent' => 'Mozilla/5.0 (etc...)' })
-      response = Net::HTTP.start(url.host, url.port, use_ssl: true) { |http| http.request(req) }
-      case response
-      when Net::HTTPSuccess     then response.body.force_encoding("UTF-8")
-      when Net::HTTPRedirection then fetch(response['location'], limit - 1)
-      else
-        response.error!
-      end
+      Utils.fetch(uri_str, limit)
     end
   end
 end
